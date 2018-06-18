fe4e914452c93949b1ba3618e6c09563cd3bdbf3
Ezwal <ezwal@github.com>
2018-06-18 22:21:21 +0200

    Fixed import script

--- import-tileset.js
+++ import-tileset.js
@@ -1,4 +1,6 @@
+#!/usr/bin/env node
 'use strict';
+
 const fs = require('fs');
 const readline = require('readline');
 
@@ -12,71 +14,79 @@ const basicSkeletton = {
 		"meta": {
 				"images": "images/tileset.png",
 				"size":{
-            "w":3147, // YOU HAVE TO SETUP THOSE MANUALLY
-            "h":2899
+            "w":32 * 10, // YOU HAVE TO SETUP THOSE MANUALLY
+            "h":32 * 10
         },
         "scale":"1"
     },
 		"frames":{}
 };
 
-const basicSprite = {
-    "frame": {
-        "x":0,
-        "y":0,
-        "w":SPRITE_OFFSET,
-        "h":SPRITE_OFFSET
-    },
-    "rotated":false,
-    "trimmed":false,
-    "spriteSourceSize":{
-        "x":0,
-        "y":0,
-        "w":SPRITE_OFFSET,
-        "h":SPRITE_OFFSET
-    },
-    "sourceSize":{
-        "w": SPRITE_OFFSET,
-        "h": SPRITE_OFFSET
-    }
-};
-
-let actualSketletton;
+let actualSkeletton;
 let spriteNb = 0;
 
 // if the file at meta.images exists then it will read the Object and then populate basicSketton
 function enterAndRead() {
-		try {
-				actualSketletton = JSON.stringify(fs.readFileSync(basicSkeletton.meta.images));
-				spriteNb = actualSketletton.frames ? Object.keys(actualSketletton.frames).length : 0;
-				} catch (e) {
-						console.log(`Warning basic skeletton will be saved at ${basicSkeletton.meta.images}`);
-						actualSketletton = basicSkeletton;
-				}
+    try {
+        actualSkeletton = JSON.stringify(fs.readFileSync(basicSkeletton.meta.images));
+        spriteNb = actualSkeletton.frames ? Object.keys(actualSkeletton.frames).length : 0;
+    } catch (e) {
+        console.log(`Warning basic skeletton will be saved at ${basicSkeletton.meta.images}`);
+        actualSkeletton = basicSkeletton;
+    }
 }
 
 // at the end of the edition round we just overwrite the whole Object to the tileset name writte in meta.images
-function writeAndLeave() {
-		fs.writeFileSync(actualSketletton.meta.images, JSON.stringify(actualSketletton));
-		process.exit(0);
+function writeSkeletton() {
+		fs.writeFileSync(actualSkeletton.meta.images, JSON.stringify(actualSkeletton));
 }
 
 enterAndRead();
-// assuming that the img is a fixed size for every sprite and all images are contigous
-for(;;) {
-    const numberOfSpritePerline = actualSketletton.meta.size.w / SPRITE_OFFSET;
-		rl.question('What sprite to add to the tileset (with extention)(quit to stop and write file)?', answer => {
-				if (answer === 'quit') {
-						writeAndLeave();
-				}
-				spriteNb += 1;
-				console.log(`Writing for the ${spriteNb}nth (top => bottom; left => right`);
+const numberOfSpritePerline = actualSkeletton.meta.size.w / SPRITE_OFFSET;
 
-				const addedSprite = basicSprite;
-				addedSprite.frame.x = (spriteNb % numberOfSpritePerline) * SPRITE_OFFSET;
-				addedSprite.frame.y = Math.floor(spriteNb / numberOfSpritePerline) * SPRITE_OFFSET;
+async function loop() {
+    return new Promise((resolve, reject) => {
+        rl.question('What sprite to add to the tileset (with extension)(quit to stop and write file)?', answer => {
+            if (answer === 'quit') {
+                writeSkeletton();
+                return reject();
+            }
+            spriteNb += 1;
+            console.log(`Writing for the ${spriteNb}nth (top => bottom; left => right)`);
 
-				actualSketletton[answer] = addedSprite;
-				rl.close();
-		});
+            actualSkeletton[answer] = {
+                frame: {
+                    x: (spriteNb % numberOfSpritePerline) * SPRITE_OFFSET,
+                    y: Math.floor(spriteNb / numberOfSpritePerline) * SPRITE_OFFSET,
+                    w: SPRITE_OFFSET,
+                    h: SPRITE_OFFSET
+                },
+                rotated: false,
+                trimmed: false,
+                spriteSourceSize: {
+                    x: 0,
+                    y: 0,
+                    w: SPRITE_OFFSET,
+                    h: SPRITE_OFFSET
+                },
+                sourceSize: {
+                    w:  SPRITE_OFFSET,
+                    h:  SPRITE_OFFSET
+                }
+            }
+            console.log(actualSkeletton);
+            return resolve();
+        });
+    });
 }
+
+// quick and dirty
+(async () => {
+    while (true) {
+        try {
+            await loop();
+        } catch (e) {
+            process.exit(0);
+        }
+    }
+})();
